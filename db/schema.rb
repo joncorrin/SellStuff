fce6e773ef310018783f6ce121e69e0df3db49a1
joncorrin <jcorrin@udel.edu>
2017-02-08 21:55:30 -0500

    Enable users to place orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208025402) do
+ActiveRecord::Schema.define(version: 20170209023900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 20170208025402) do
     t.datetime "image_updated_at"
     t.string   "image"
     t.integer  "user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
+    t.integer  "listing_id"
   end
 
   create_table "users", force: :cascade do |t|
